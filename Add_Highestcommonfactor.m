5ec33779714083b254d0aaaddb178e8cf1220af2
dushenda <dushenda1996@163.com>
2019-04-09 22:10:52 +0800

    '解决第五问'

--- Add_Highestcommonfactor.m
+++ Add_Highestcommonfactor.m
@@ -1,15 +1,10 @@
-% 找出能够整除 1~20 的数
-clear;clc;
-x=1:20;
-y = lcm(x);
-disp(['结果为:',num2str(y)]);
-function leastcommonmultiple = lcm(x)
-    leastcommonmultiple=x(1);
+% 辗转相除法,只能求两个数的最大公约数
+function nHighestcommonfactor = Add_Highestcommonfactor(x)
+    nHighestcommonfactor=x(1);
     for i=2:max(size(x))
-        leastcommonmultiple = leastcommonmultiple*x(i)/Add_Highestcommonfactor2num(leastcommonmultiple,x(i));
+        nHighestcommonfactor = Add_Highestcommonfactor2num(nHighestcommonfactor,x(i));
     end
 end
-
 
 function Highestcommonfactor = Add_Highestcommonfactor2num(x,y)
     if(x<y)
