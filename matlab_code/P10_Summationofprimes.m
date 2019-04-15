406ead5c8889e811bdf49c917117031b09c08c7e
dushenda <dushenda1996@163.com>
2019-04-15 14:51:00 +0800

    P10

--- matlab_code/P10_Summationofprimes.m
+++ matlab_code/P10_Summationofprimes.m
@@ -1,17 +1,17 @@
-clear;clc;
-% 埃拉托尼素数筛选法，复杂度O(nlognlogn)
-limit = 2000000;
-crosslimit = ceil(sqrt(limit));
-for n=4:2:limit
-    sieve(n) = true;
-end
-for n=3:2:crosslimit
-    if not sieve[n] then // n not marked, hence prime
-        for m := n*n to limit with step 2*n
-        sieve[m] := true
-        end for
-    end if
-end
+% clear;clc;
+% % 埃拉托尼素数筛选法，复杂度O(nlognlogn)
+% limit = 2000000;
+% crosslimit = ceil(sqrt(limit));
+% for n=4:2:limit
+%     sieve(n) = true;
+% end
+% for n=3:2:crosslimit
+%     if not sieve[n] then // n not marked, hence prime
+%         for m := n*n to limit with step 2*n
+%         sieve[m] := true
+%         end for
+%     end if
+% end
 % 内置函数，贼快
 tic
 sum(primes(2000000))
